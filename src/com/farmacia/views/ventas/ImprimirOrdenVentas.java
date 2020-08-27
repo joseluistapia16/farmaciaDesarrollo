bc0d19df20759aa429d202cfbdd4ba2b969aa5bc
iabotprueba <63275364+iabotprueba@users.noreply.github.com>
2020-08-27 13:27:16 -0500

    commit
    arreglo error de impresion(comentado)

--- src/com/farmacia/views/ventas/ImprimirOrdenVentas.java
+++ src/com/farmacia/views/ventas/ImprimirOrdenVentas.java
@@ -166,7 +166,7 @@ public class ImprimirOrdenVentas extends javax.swing.JDialog {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-
+/*
         try {
             Conexion con = new Conexion();
 
@@ -199,7 +199,7 @@ public class ImprimirOrdenVentas extends javax.swing.JDialog {
         } catch (ClassNotFoundException | JRException ex) {
             Logger.getLogger(ImprimirOrdenVentas.class.getName()).log(Level.SEVERE, null, ex);
         }
-
+*/
 
     }//GEN-LAST:event_jButton1ActionPerformed
 
